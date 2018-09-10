52beea901b592c17a9d99f3c90251405987837d2
HoaiDuong <hoaiduong1012@gmail.com>
2018-09-10 23:38:43 +0700

    commit thu

--- controllers/logincontroller.js
+++ controllers/logincontroller.js
@@ -3,14 +3,14 @@ var login = angular.module('login', []);
 
 login.controller('logincontroler', function ($scope) {
 
-    var a='';
-    $scope.username;
+
+    $scope.username='';
     $scope.password='';
     $scope.login=function () {
 
 
 
-        console.log(typeof $scope.username);
+
 
 
     }
--- server.js
+++ server.js
@@ -1,40 +1,56 @@
 var express= require('express');
 var app=express();
 var path = require('path');
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'duong..l',
+    database : 'DuongTest',
+
+
+});
+
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'controllers')));
+
 app.listen(3000,function(){
     console.log('server is running');
 });
- app.set('views', './views');
- app.set('view engine', 'ejs');
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname, 'views/index.html'));
-});
 
-app.get('/api/getSinhViens',function (req,res) {
-    var sinhvien={
-        sid:1,
-        name:'Tao Hoai Duong',
-        number:'0169 666 0204',
-        email:'hoaiduong1012@gmail.com'
-
-    }
-
-    res.send(JSON.stringify(sinhvien));
 });
 
 
+app.get('/api/sinhviens',function (req,res) {
+    connection.connect();
 
-app.get('/api/hello',function (req,res) {
-
-
-    res.send('xin chao cac ban');
+    connection.query('select * from sinh_vien',function (err, rows, fields) {
+        console.log(err);
+        res.send(rows);
+    })
+    
+    connection.end();
 })
+
+
+
+
+
+
+
+
+
 
 
 
